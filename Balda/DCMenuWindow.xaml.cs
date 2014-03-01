e5bcb38cda420f90a7964618418369b60a5416e1
Denchyk <Denchyk@localhost>
2014-03-01 04:33:28 +0200

    Creating transition between winows

---
+++ Balda/DCMenuWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Balda
+{
+    /// <summary>
+    /// Логика взаимодействия для DCMenuWindow.xaml
+    /// </summary>
+    public partial class DCMenuWindow : Window
+    {
+        public DCMenuWindow()
+        {
+            InitializeComponent();
+            
+        }
+        private void buttonLoadGame_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonNewGame_Click(object sender, RoutedEventArgs e)
+        {
+            DCSettingGameWindow settingGameWindow = new DCSettingGameWindow();
+            settingGameWindow.Show();
+
+        }
+    }
+}
---
+++ Balda/DCSettingGameWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Balda
+{
+    /// <summary>
+    /// Логика взаимодействия для DCSettingGameWindow.xaml
+    /// </summary>
+    public partial class DCSettingGameWindow : Window
+    {
+        public DCSettingGameWindow()
+        {
+            InitializeComponent();
+            listboxDifficulty.Items.Add("Easy");
+            listboxDifficulty.Items.Add("Normal");
+            listboxDifficulty.Items.Add("Hard");
+           
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void listboxDifficulty_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void checkBoxPalyers_Checked(object sender, RoutedEventArgs e)
+        {
+           
+                textBoxPlayerTwoName.IsEnabled = true;
+            listboxDifficulty.IsEnabled = false;
+        }
+
+        private void checkBoxPalyers_Unchecked(object sender, RoutedEventArgs e)
+        {
+            textBoxPlayerTwoName.IsEnabled = false;
+        }
+
+        private void buttonStart_Click(object sender, RoutedEventArgs e)
+        {
+            DCGameWindow gameWindow = new DCGameWindow();
+            gameWindow.Show();
+        }
+    }
+}
