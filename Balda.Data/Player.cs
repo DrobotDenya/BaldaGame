919a294026dc852c747c7391aa2d608200eec31b
Denchyk <Denchyk@localhost>
2014-03-09 23:14:46 +0200

    Adding class Player

---
+++ Balda.Data/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balda.Data
+{
+    public sealed class  Player
+    {
+        private readonly string _name;
+        private readonly string _sername;
+        private readonly string _password;
+
+        public string getName()
+        {
+            return _name;
+        }
+
+        private static Player player;
+
+        private Player() { }
+
+        public static Player sharePlayer
+        {
+            get
+            {
+                if (player == null)
+                {
+                    player = new Player();
+                }
+
+                return player;
+            }
+        }
+    }
+}
