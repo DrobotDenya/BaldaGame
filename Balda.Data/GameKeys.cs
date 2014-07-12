c4a3349f51cd6f5a61bd96491069f7c6435d6fde
Denchyk <Denchyk@localhost>
2014-07-12 20:38:38 +0300

    Add keyboard and Dictionary

---
+++ Balda.Data/GameKeys.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace Balda.Data
+{
+    public class GameKeys
+    {
+        ArrayList keys = new ArrayList();
+        string path = @"C:\Users\drobo_000\Documents\HG\Balda-clone\ia23-09-Balda\Resources\Keyboard.txt";
+        
+        public GameKeys()
+        {
+            readKeyBoard();
+        }
+
+        private void readKeyBoard()
+        {
+            string line;
+            StreamReader file = new StreamReader(path); 
+            while((line = file.ReadLine()) != null)
+            {
+                keys.Add(line);
+            }
+            file.Close();
+        }
+        
+
+
+
+    }
+}
