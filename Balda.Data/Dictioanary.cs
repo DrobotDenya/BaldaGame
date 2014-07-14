653704f1b530ed255ae31156086961bd9d5c9407
Denchyk <Denchyk@localhost>
2014-07-14 19:55:31 +0300

    Create MVVM logig

---
+++ Balda.Data/Dictioanary.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Balda.Data
+{
+    public class Dictionary
+    {
+        List<string> words = new List<string>();
+
+        public Dictionary()
+        {
+            string word;
+            string fileName = @"C:\Users\drobo_000\Documents\HG\Balda-clone\ia23-09-Balda\Resources\words.txt";
+            StreamReader file = new StreamReader(fileName);
+            while((word = file.ReadLine()) != null)
+            {
+                if (!words.Contains(word))
+                {
+                    words.Add(word);
+ 
+                }
+            }
+            file.Close();
+
+        }
+
+        public int getCount()
+        {
+            return words.Count;
+        }
+
+        public List<string> getDictionary()
+        {
+            return words;
+        }
+
+        public string getRandomWord(int length)
+        {
+            List<string> listwords = getWords(length);
+            if (listwords.Count > 0)
+            {
+                Random rand = new Random();
+                int randomNumb = rand.Next(0 ,listwords.Count);
+                return listwords[randomNumb];                 
+            }
+            return null;
+ 
+        }
+
+        public List<string> getWords(int length)
+        {
+            List<string> listWords = new List<string>();
+
+            foreach (string word in words)
+            {
+                if (word.Length == length)
+                {
+                    listWords.Add(word);
+                }
+            }
+            return listWords;
+        }
+
+        public bool isExist(string word)
+        {
+            return words.Contains(word);
+        }
+
+        public bool isCanExist(string letters)
+        {
+            foreach (string word in words)
+            {
+                if (word.IndexOf(letters) != -1)//????????
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public List<string> possibleWords(string letters)
+        {
+            List<string> words = new List<string>();
+            foreach (string word in words)
+            {
+                if (word.Substring(1).IndexOf(word) != -1)
+                {
+                    words.Add(word);
+                }
+
+                if (word.Substring(0, word.Length - 2).IndexOf(word) != -1)
+                {
+                    words.Add(word);
+ 
+                }
+            }
+            return words;
+        }
+
+
+        
+    }
+}
---
+++ Balda.Data/GameBoard.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Balda.Data
+{
+    public class GameBoard
+    {
+        int _width;
+        int _heigth;
+
+        string [,] cellPool;
+        public GameBoard(int width, int heigth)
+        {
+            setSize(width, heigth);
+        }
+
+        public GameBoard()
+        {
+ 
+        }
+        //set size for board
+        public void setSize(int width, int heigth)
+        {
+            this._width = width;
+            this._heigth = heigth;
+            
+           cellPool = new string[width, heigth];
+            for (int row = 0; row < heigth; row++)
+            {
+                for(int column = 0; column < width; column++)
+                {
+                    
+                    cellPool[row ,column] = "";
+                }               
+            }
+        }
+
+        //Retutn width
+        public int width()
+        {
+            return this._width;
+        }
+
+        //Return heigth
+        public int heigth()
+        {
+            return this._heigth;
+        }
+        
+        //Clear gameBoard
+        public void clear()
+        {
+            cellPool = new string[_width, _heigth];
+        }
+
+        //Return value cell
+        public string getCellValue(int row, int column)
+        {
+            return cellPool[row,column];
+        }
+
+        //Set value for cell
+        public void setCellValue(string value, int row, int column)
+        {
+            cellPool[row ,column] = value;
+        }
+
+
+
+    }
+}
